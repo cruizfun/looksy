f1f5bdb197414b41d866ab02929e6368bc8ef44a
cruizfun <cruizfun@chicagobooth.edu>
2020-01-04 18:46:04 +0000

    working on passing variables

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,5 +1,6 @@
 class VotesController < ApplicationController
   def create
+
     @vote = Vote.new
     @vote.voter_id = session[:user_id]
     @vote.post_id = params.fetch("post_id_from_query")
@@ -9,6 +10,8 @@ class VotesController < ApplicationController
       redirect_to("/feed", { :notice => "Vote failed to create successfully." })
     else
       @vote.save
+      @vote1 = @vote.where({ :voter_id => @current_user.id, :bestoutfit_id => @post.outfit1_id }).first
+      @vote2 = @vote.where({ :voter_id => @current_user.id, :bestoutfit_id => @post.outfit2_id }).first
       respond_to do |format|  
         format.js { render 'posts/feed_partial'}
       end
