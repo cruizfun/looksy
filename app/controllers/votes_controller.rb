e56cf3f4e9d88c05ddb710c52494624d2319a11c
cruizfun <cruizfun@chicagobooth.edu>
2020-01-04 20:15:39 +0000

    functioning first post not others

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -4,17 +4,23 @@ class VotesController < ApplicationController
     @vote = Vote.new
     @vote.voter_id = session[:user_id]
     @vote.post_id = params.fetch("post_id_from_query")
-    @vote.bestoutfit_id = params.fetch("bestoutfit_id_from_query")
 
-    if @vote.post_id == nil
-      redirect_to("/feed", { :notice => "Vote failed to create successfully." })
-    else
+    if params.fetch("bestoutfit1_id_from_query", "none") != "none"
+      @vote.bestoutfit_id = params.fetch("bestoutfit1_id_from_query")
       @vote.save
-      @vote1 = @vote.where({ :voter_id => @current_user.id, :bestoutfit_id => @post.outfit1_id }).first
-      @vote2 = @vote.where({ :voter_id => @current_user.id, :bestoutfit_id => @post.outfit2_id }).first
+      @vote1 = 1
       respond_to do |format|  
-        format.js { render 'posts/feed_partial'}
+      format.js { render 'posts/feed_partial'}
       end
+    elsif params.fetch("bestoutfit2_id_from_query", "nonee") != "none"
+      @vote.bestoutfit_id = params.fetch("bestoutfit2_id_from_query")
+      @vote.save
+      @vote2 = 1
+      respond_to do |format|  
+      format.js { render 'posts/feed_partial'}
+      end
+    elsif @vote.post_id == nil
+      redirect_to("/feed", { :notice => "Vote failed to create successfully." })
     end  
   end
 end
